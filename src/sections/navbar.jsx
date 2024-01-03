9a18dc101f13c5d7a3364daf0ade3305781377d4
Pasin <mossps@Pasins-MacBook-Pro.local>
2024-01-04 00:12:24 +0700

    3

--- src/sections/navbar.jsx
+++ src/sections/navbar.jsx
@@ -18,21 +18,10 @@ const Navbar = () => {
       sectionElement.scrollIntoView({ behavior: "smooth" });
     }
   };
-
-  const handleMouseMove = (event) => {
-    const yOffset = event.clientY - arrowRef.current.getBoundingClientRect().top;
-    arrowRef.current.style.top = `${yOffset}px`;
-  };
-
   return (
-    <div className="flex flex-col gap-3 font-semibold mt-2 relative" onMouseMove={handleMouseMove}>
-      <FontAwesomeIcon
-        ref={arrowRef}
-        className="mr-2 text-primaryTitle absolute transition-all duration-500"
-        icon={faArrowRight}
-        style={{ display: 'none' }}
-      />
-      {navs.map((e, i) => (
+    <div className="flex flex-col gap-3 font-semibold mt-2 relative" >
+      {
+      navs.map((e, i) => (
         <div
           key={`nav-${e}-${i}`}
           onClick={() => handleClick(e.sectionId)}
